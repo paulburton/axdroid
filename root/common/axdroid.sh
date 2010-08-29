ed5cacbd7972eb862557a38018a91b19c78034b1
Paul Burton <paulburton89@gmail.com>
2010-08-29 20:07:31 +0100

    WiFi setup

--- root/common/axdroid.sh
+++ root/common/axdroid.sh
@@ -4,6 +4,9 @@ echo "Axdroid realroot init"
 
 DEBUG=1
 
+insmod /lib/modules/acx-mac80211.ko
+#insmod /lib/modules/aximx50_acx.ko
+
 if [ $DEBUG -eq 1 ]
 then
 	ifconfig usb0 10.0.0.2 netmask 255.255.255.0 up
