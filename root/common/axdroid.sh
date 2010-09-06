07d78dce1918efa3b1a2088f70ffbe40e8cfb2c4
Paul Burton <paulburton89@gmail.com>
2010-09-06 20:14:11 +0100

    Include /system/bin in telnetd PATH
    This makes it more convenient to execute binaries in /system/bin over the telnet debug shell

--- root/common/axdroid.sh
+++ root/common/axdroid.sh
@@ -10,7 +10,7 @@ insmod /lib/modules/acx-mac80211.ko
 if [ $DEBUG -eq 1 ]
 then
 	ifconfig usb0 10.0.0.2 netmask 255.255.255.0 up
-	telnetd -l /bin/sh
+	PATH="$PATH:/system/bin" telnetd -l /bin/sh
 fi
 
 /init
