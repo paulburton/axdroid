ffde9ef68b8decb3d6191f4c972dcb25816c27ec
Paul Burton <paulburton89@gmail.com>
2010-09-13 23:20:27 +0100

    Improvements to the init scripts
    Checks more error conditions, cleans up the console redirect mess somewhat, does more from the real root

--- root/common/axdroid.sh
+++ root/common/axdroid.sh
@@ -3,14 +3,35 @@
 echo "Axdroid realroot init"
 
 DEBUG=1
+COMPCACHE=1
+
+echo /sbin/axdroid_hotplug >/proc/sys/kernel/hotplug
 
 insmod /lib/modules/acx-mac80211.ko
 insmod /lib/modules/aximx50_acx.ko
+
+if [ $COMPCACHE -eq 1 ]
+then
+	insmod /lib/modules/ramzswap.ko num_devices=1
+fi
 
 if [ $DEBUG -eq 1 ]
 then
 	ifconfig usb0 10.0.0.2 netmask 255.255.255.0 up
 	PATH="$PATH:/system/bin" telnetd -l /bin/sh
+fi
+
+if [ $DEBUG -eq 0 ]
+then
+	VGA=`cat /proc/mtd | grep "Video SDRAM" | wc -l`
+	if [ $VGA -eq 1 ]
+	then
+		cp /initlogoVGA.rle /initlogo.rle
+	else
+		cp /initlogoQVGA.rle /initlogo.rle
+	fi
+else
+	rm -f /initlogo.rle
 fi
 
 /init
