9d9fe5f6976a90bc9df908ed4639a18b5995faa2
Paul Burton <paulburton89@gmail.com>
2010-09-15 21:41:06 +0100

    Switch to kernel 2.6.35

--- build.sh
+++ build.sh
@@ -147,7 +147,7 @@ downloadTheCode()
 		(
 			set -e
 			cd src
-			git clone git://github.com/paulburton/axdroid_kernel.git kernel
+			git clone git://github.com/paulburton/axdroid_kernel.git kernel -b android-2.6.35
 		) || exit 1
 	fi
 
